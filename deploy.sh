14006431bffafdaca9d2e036f4c87c2dabdc2a8c
rissole <bonzaibrother@gmail.com>
2021-12-11 14:47:08 +1100

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 hugo --minify
 git commit -am "deploy"
-git subtree push --prefix public origin gh-pages
+git push origin `git subtree split --prefix public main`:gh-pages --force-with-lease
 git push origin main:main
