6a3c8f6b4809fe59e84be038ebfa2344d7c159fe
rissole <bonzaibrother@gmail.com>
2021-12-12 18:01:48 +1100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
+# run in root folder on main branch
 hugo --minify
+git add .
 git commit -am "deploy"
 git push origin `git subtree split --prefix public main`:gh-pages --force-with-lease
 git push origin main:main
